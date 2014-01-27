c327c7bd968e6033fb8449e5e84ba2dc224415c1
Benjamin Berlin <benjamin.berlin@activision.com>
2014-01-26 22:11:34 -0800

    Adding dying

--- rpg.js
+++ rpg.js
@@ -19,12 +19,29 @@ var character = Backbone.Model.extend({
 	},
 	attack : function(myEnemy){
   	var z = this;
+  	debug(z.get("name")+" attacked "+myEnemy.get("name"));
   	myEnemy.hit(z.get("attack"));
+  	myEnemy.update();
 	},
 	hit : function(damage){
     var z = this,
     hp = z.get("hp");
   	z.set({"hp":hp - damage});
+  	if(hp-damage <= 0){
+    	z.die();
+  	}
+	},
+	update : function(){
+  	var z = this;
+  	if(z.get("hp") <= 0){
+    	$("."+z.get("id")+"_"+z.get("name")).hide().remove();
+  	}else {
+  	  $("."+z.get("id")+"_"+z.get("name")).html(z.get("name")+" / "+z.get("hp"));
+  	}
+	},
+	die : function(){
+  	var z = this;
+  	debug(z.get("name")+" died");
 	}
 });
 var enemy = character.extend({
@@ -32,10 +49,18 @@ var enemy = character.extend({
     var z = this;
     debug(z.get("name")+"'s Turn");
     
-    var num = roll(w.pcs.length);
+    var num = roll(w.pcs.length) - 1;
     console.log(num, w.pcs);
     z.attack(w.pcs[num]);
-    debug(z.get("name")+" attacked "+w.pcs[num].get("name"));
+    //debug(z.get("name")+" attacked "+w.pcs[num].get("name"));
+    
+    w.nextTurn();
+  },
+  die : function(){
+    w.battleOrder.splice(w.battleOrder.indexOf(this));
+    w.enemies.splice(w.enemies.indexOf(this));
+    this.update();
+    debug(this.get("name")+" died");
   }
 });
 var playerCharacter = character.extend({
@@ -48,7 +73,25 @@ var playerCharacter = character.extend({
     debug(z.get("name")+"'s Turn");
     
     w.menu.find(".title").html(z.get('name')+"'s Turn");
-    w.menu.find(".actions").html("<li class='attack'>Attack</li><li class='defend'>Defend</li>");
+    w.menu.find(".actions").html("<li class='action attack'>Attack</li><li class='action defend'>Defend</li>");
+    $(".action").click(function(){
+      var action = $(this);
+      if(action.hasClass("attack")){
+        var enemies = w.enemies;
+        console.log(enemies);
+        $.each(enemies, function(i, e){
+          $(".attack").append("<span class='enemy' rel='"+i+"'>"+e.get("name")+"</span>");
+        });
+        $(".enemy").click(function(){
+          $(".enemy").remove();
+          z.attack(w.enemies[$(this).attr("rel")]);
+          w.nextTurn();
+        });
+        
+      } else {
+        w.nextTurn();
+      }
+    });
   }
 });
 
@@ -56,6 +99,7 @@ var world = Backbone.Model.extend({
 	pcs : [],
 	enemies : [],
 	battleOrder : [],
+	currentTurn : 0,
 	pcWorld : $("#pcs"),
 	enemyWorld : $("#enemies"),
 	menu: $("#menu"),
@@ -106,18 +150,30 @@ var world = Backbone.Model.extend({
     	return b.get('currentInitiative') - a.get('currentInitiative');
   	});
   	
-  	z.battleOrder[0].myTurn();
+  	z.currentTurn = 0;
+  	z.battleOrder[z.currentTurn].myTurn();
   	
   	//setTimeout(function(){
   	console.log(z.battleOrder);
   	//}, 1000);
+	},
+	
+	nextTurn : function(){
+	  var z = this;
+  	if(z.currentTurn >= z.battleOrder.length){
+    	z.currentTurn = 0;
+  	} else {
+    	z.currentTurn++;
+  	}
+  	
+  	z.battleOrder[z.currentTurn].myTurn();
 	}
 });
 
 var roll = function(sides=20, num=1, add=0){
   var total = 0;
   for(var k = 0; k < num; k++){
-    total += Math.floor(Math.random()*(sides+1));
+    total += Math.floor(Math.random()*(sides));
   }
   return total + add;
 }
